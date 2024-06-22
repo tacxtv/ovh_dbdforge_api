8d2f84680c2e1f3278d65e8db6cc5d7f6e6f44a8
Tacx <12997062+tacxou@users.noreply.github.com>
2024-06-22 15:07:14 +0200

    feat: Add RoleEnum and Character schema for wiki characters

--- src/wiki/characters/characters.service.ts
+++ src/wiki/characters/characters.service.ts
@@ -1,6 +1,10 @@
 import { Injectable } from "@nestjs/common"
+import { InjectModel } from "@nestjs/mongoose"
 
 @Injectable()
-export class CharactersService {
+export class CharactersService extends AbstractServiceSchema<Addon, AddonDto> {
+  public constructor(@InjectModel(Addon.name) protected readonly _model: Model<Addon>) {
+    super()
+  }
 
 }
